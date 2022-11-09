5341ca7463a4e4141154c5f39909c7c22d64a2c5
mycooldj.eth <58099602+MyCool-DJ@users.noreply.github.com>
2022-11-09 05:03:29 +0000

    Added more to the UI

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
-def colorChange(color): #subrountine
-	if color=="red":
-		return("\33[31m")
-	elif color=="white":
-		return("\033[0m")
-	elif color=="yellow":
-		return("\033[33m")
-	elif color=="green":
-		return("\033[32m")
-	elif color=="purple":
-		return("\033[35m")
+def colorChange(color): #subrountine 
+  if color=="red":
+    return ("\033[31m")
+  elif color=="white":
+    return ("\033[0m")
+  elif color=="blue":
+    return ("\033[34m")
+  elif color=="yellow":
+    return ("\033[33m")
+  elif color == "green":
+    return ("\033[32m")
+  elif color == "purple":
+    return ("\033[35m")
 		
-title = f"{colorChange('red')} = {colorChange('white')}=
- {colorChange('blue')}={colorChange('yellow)'}Music App {colorChange('blue')}={colorChange('white')}={colorChange('red')}="
+title = f"{colorChange('red')}={colorChange('white')}={colorChange('blue')}= {colorChange('yellow')}Music App {colorChange('blue')}={colorChange('white')}={colorChange('red')}="
 
-print(f"					{title:^35}")
- 
-	
+print(f"	      	{title:^35}")
+print()
+print(f">\t{colorChange('white')}Radio Gaga")
+print(f"\t{colorChange('yellow')}Queen")
+
+prev = "prev"
+next = "next"
+pause = "pause"
+
+print(f"{colorChange('white')}{prev:<35}")
+print(f"{colorChange('green')}{next:^35}")
+print(f"{colorChange('purple')}{pause:>35}")
+
+
+print()
+print()
+text = "WELCOME TO"
+print(f"{colorChange('white')}{text:^35}")
+text = "-- ARMBOOK --"
+print(f"{colorChange('blue')}{text:^35}")
+text = "Definitely not a rip off"
+print(f"{colorChange('yellow')}{text:>35}")
